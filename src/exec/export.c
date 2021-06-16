da715f3caf13c24b7cca1349fd9fdc26f3e14836
aldubar <aldubar@student.42.fr>
2021-06-16 18:11:16 +0200

    ajout check env var name

--- src/exec/export.c
+++ src/exec/export.c
@@ -1,21 +1,46 @@
 #include "minishell/minishell.h"
 #include "minishell/env.h"
 #include "libft/cstring.h"
+#include "libft/ctype.h"
 #include <stdlib.h>
+#include "libft/io.h"
+
+static int	check_var_name(char *s)
+{
+	int		i;
+
+	i = 0;
+	if (ft_isdigit(s[0]))
+		return (0);
+	while (s[i])
+	{
+//		if (ft_isdigit(s[i]) || ft_isupper(s[i]) || s[i] == '_')
+		if (ft_isdigit(s[i]) || (s[i] >= 'A' && s[i] <= 'Z') || s[i] == '_')
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
 
 void	fn_export(char **ag, size_t len)
 {
 	char	*var_name;
 	char	*s;
-	
+
 	if (len == 2)
 	{
 		s = ft_strdup(ag[1]);
 		var_name = s;
-		while (*s != '=')
-			++s;
-		*s++ = '\0';
-		minishell_setenv(var_name, s);
+		s = ft_strchr(s, '=');
+		if (s == NULL)
+			s = "";
+		else
+			*s++ = '\0';
+		if (check_var_name(var_name))
+			minishell_setenv(var_name, s);
+		else
+			ft_dprintf(2, "minishel: export \"%s\" : identifiant non valable\n", var_name);
 		free(var_name);
 	}
 }

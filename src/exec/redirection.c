4ed3d511d4bc8983ffe7e116dd3d93e0d1a44ae9
abrabant <abrabant@student.42.fr>
2021-06-25 13:06:57 +0200

    Remove file descriptor debug in redirections

--- src/exec/redirection.c
+++ src/exec/redirection.c
@@ -82,64 +82,9 @@ int	make_redirections(t_vector redirv, int pipefd[2], size_t index, size_t lengt
 	i = 0;
 	while (i <= index)
 	{
-		ft_dprintf(2, "Close %d\n", (pipefd + (i * 2))[0]);
-		ft_dprintf(2, "Close %d\n", (pipefd + (i * 2))[1]);
 		close_safe(pipefd + (i * 2));
 		close_safe(pipefd + (i * 2) + 1);
 		i++;
 	}
 	return (make_file_redirections(redirv));
 }
-
-/*
-int	open_in(t_command *cmd, int *fd_in)
-{
-	size_t			i;
-	t_redirection	*redir;
-
-	*fd_in = -1;
-	i = 0;
-	while (i < ft_vector_length(cmd->redir_in))
-	{
-		if (i > 0)
-			close(*fd_in);
-		redir = ft_vector_get(cmd->redir_in, i);
-		*fd_in = open(redir->arg, O_RDONLY);
-		if (*fd_in == -1)
-		{
-			ft_dprintf(STDERR_FILENO, "minishell: %s: %s\n",
-					redir->arg, strerror(errno));
-			return (1);
-		}
-		++i;
-	}
-	return (0);
-}
-
-int	open_out(t_command *cmd, int *fd_out)
-{
-	size_t			i;
-	t_redirection	*redir;
-
-	*fd_out = -1;
-	i = 0;
-	while (i < ft_vector_length(cmd->redir_out))
-	{
-		if (i > 0)
-			close(*fd_out);
-		redir = ft_vector_get(cmd->redir_out, i);
-		if (redir->type == REDIRECTION_DOUT)
-			*fd_out = open(redir->arg, O_RDWR | O_APPEND | O_CREAT, 0644);
-		else
-			*fd_out = open(redir->arg, O_RDWR | O_TRUNC | O_CREAT, 0644);
-		if (*fd_out == -1)
-		{
-			ft_dprintf(STDERR_FILENO, "minishell: %s: %s\n",
-					redir->arg, strerror(errno));
-			return (1);
-		}
-		++i;
-	}
-	return (0);
-}
-*/

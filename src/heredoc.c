a04c8eedba4e23f0804d9ba229f53bffc91954b3
abrabant <abrabant@student.42.fr>
2021-06-26 21:07:29 +0200

    Implement here document togglable variable expansion

--- src/heredoc.c
+++ src/heredoc.c
@@ -8,9 +8,13 @@
 
 #include "minishell/minishell.h"
 #include "minishell/stat.h"
+#include "minishell/error.h"
+#include "minishell/env.h"
 
+#include "libft/string.h"
 #include "libft/cstring.h"
 #include "libft/io.h"
+#include "libft/ctype.h"
 
 static size_t	get_linecount(void)
 {
@@ -32,31 +36,84 @@ static size_t	get_linecount(void)
 	return (lc);
 }
 
-int	here_doc_prompt(const char *delim)
+static void	expand_var(t_string *str, char **line_loc)
+{
+	size_t	i;
+	char	*line;
+	char	*var_name;
+	char	*var_val;
+
+	i = 0;
+	line = *line_loc;
+	var_val = NULL;
+	while (ft_isalnum(line[i]))
+		++i;	
+	var_name = ft_substr(line, 0, i);
+	if (var_name != NULL)
+		var_val = minishell_getenv(var_name);
+	if (var_val != NULL)
+		ft_string_append_cstr(*str, var_val);
+	*line_loc += i - 1; 
+}
+
+static char	*expand_line(char *line)
+{
+	t_string	str;
+	char		*expanded;
+
+	str = ft_string_new(ft_strlen(line));
+	while (*line != '\0')
+	{
+		if (*line == '$' && line[1] != '\0')
+		{
+			++line;
+			expand_var(&str, &line);
+		}
+		else
+			ft_string_append_char(str, *line);
+		++line;
+	}
+	expanded = ft_string_tocstring(str);
+	ft_string_destroy(str);
+	return (expanded);
+}
+
+static void	prepare_fds(int fd[2])
 {
 	char	buf[10000];
-	char	*line;
-	int		write_fd;
-	int		read_fd;
 
-	write_fd = open(HERE_DOC_FILEPATH, O_WRONLY | O_APPEND | O_CREAT, 0664);
-	read_fd = open(HERE_DOC_FILEPATH, O_RDONLY);
-	while (read(read_fd, buf, 10000) > 0)
+	fd[1] = open(HERE_DOC_FILEPATH, O_WRONLY | O_APPEND | O_CREAT, 0664);
+	fd[0] = open(HERE_DOC_FILEPATH, O_RDONLY);
+	while (read(fd[0], buf, 10000) > 0)
 		;
-	rl_replace_line("", 0);
+}
+
+int	here_doc_prompt(const char *delim)
+{
+	bool	disable_expansion;
+	char	*line;
+	int		fd[2];
+
+	prepare_fds(fd);
+	disable_expansion = (*delim == '\'' || *delim == '"')
+		&& delim[ft_strlen(delim) - 1] == *delim;
+	if (disable_expansion)
+		delim = assert_ptr(word_strip_quotes((char *)delim));
 	line = readline("> ");
 	while (line != NULL && ft_strcmp(line, delim) != 0)
 	{
-		write(write_fd, line, ft_strlen(line));
-		write(write_fd, "\n", 1);
+		if (!disable_expansion)
+			line = assert_ptr(expand_line(line));
+		write(fd[1], line, ft_strlen(line));
+		write(fd[1], "\n", 1);
 		free(line);
-		rl_on_new_line();
 		line = readline("> ");
 	}
+	write(fd[1], "\n", 1);
 	if (line == NULL)
-		ft_dprintf(STDERR_FILENO, "minishell: warning: here-document at line"
-			" %ld delimited by end-of-file (wanted `%s`)\n", get_linecount() + 1, delim);	
+		ft_dprintf(2, "minishell: warning: here-document at line %ld delimited"
+				"by end-of-file (wanted `%s`)\n", get_linecount(), delim);	
 	free(line);
-	close(write_fd);
-	return (read_fd);
+	close(fd[1]);
+	return (fd[0]);
 }

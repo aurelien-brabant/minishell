a23a5c4c79217c73ed838e8ad78b344c2f0cf099
aldubar <aldubar@student.42.fr>
2021-06-27 17:34:55 +0200

    exit pipe ok

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -7,7 +7,7 @@
 #include "libft/strconv.h"
 #include "libft/ctype.h"
 
-int	check_digit(char *s)
+static int	check_digit(char *s)
 {
 	int		i;
 	int		neg;
@@ -29,10 +29,26 @@ int	check_digit(char *s)
 	return (neg);
 }
 
+static int	check_args(char *argv)
+{
+	if (check_digit(argv) == -1)
+	{
+		ft_dprintf(STDERR_FILENO, "minishell: exit: %s : "
+			"numeric argument required\n", argv);
+		minishell_exit(2);
+		return (2);
+	}
+	else
+	{
+		ft_dprintf(STDERR_FILENO, "minishell: exit: too many arguments\n");
+		return (1);
+	}
+}
+
 int	builtin_exit(int argc, char *argv[], bool forked)
 {
-	(void)forked;
-
+	if (forked)
+		return (0);
 	ft_putstr_fd("exit\n", STDOUT_FILENO);
 	if (argc == 2)
 	{
@@ -42,20 +58,7 @@ int	builtin_exit(int argc, char *argv[], bool forked)
 			minishell_exit((256 + ft_atoi(argv[1]) % 256));
 	}
 	else if (argc >= 2)
-	{
-		if (check_digit(argv[1]) == -1)
-		{
-			ft_dprintf(STDERR_FILENO, "minishell: exit: %s : "
-				"numeric argument required\n", argv[1]);
-			minishell_exit(2);
-			return (2);
-		}
-		else
-		{
-			ft_dprintf(STDERR_FILENO, "minishell: exit: too many arguments\n");
-			return (1);
-		}
-	}
+		return (check_args(argv[1]));
 	minishell_exit(0);
 	return (0);
 }

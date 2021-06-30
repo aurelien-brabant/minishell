a3da85a5769a3c5e0cfa4fe7676e9d65e45d09df
aldubar <aldubar@student.42.fr>
2021-06-30 19:55:18 +0200

    unset clean

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -8,16 +8,9 @@
 
 int	builtin_unset(int argc, char *argv[], bool forked)
 {
-	char	*s;
-	char	*sep;
-
 	(void)forked;
 	if (argc == 2)
 	{
-		s = ft_strdup(argv[1]);
-		sep = ft_strchr(s, '=');
-		if (sep != NULL)
-			*sep = '\0';
 		if (argv[1][0] == '=' || !is_valid_env_var_name(argv[1]))
 		{
 			ft_dprintf(2, "minishell: unset \"%s\": not a valid"
@@ -25,8 +18,7 @@ int	builtin_unset(int argc, char *argv[], bool forked)
 			return (1);
 		}
 		else
-			minishell_unsetenv(s);
-		free(s);
+			minishell_unsetenv(argv[1]);
 	}
 	return (0);
 }
